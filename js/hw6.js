008647a5d6b74a9c8ea8a74457a0ca06ce0cd5c7
Paprawin Boonyakida <paprawin@hotmail.com>
2020-11-10 22:08:16 -0500

    updated hw6

--- js/hw6.js
+++ js/hw6.js
@@ -7,91 +7,100 @@ paprawin_boonyakida@student.uml.edu
 Note: Uses event listener to validate input and generate table
 */
 
-var tabID = "table"; // indicate table id from html here
+// indicate table id from html here
+var tabID = "table"; 
 
 /*Test to determine if column max > min to flag the error*/
 $.validator.addMethod("colTest", function(value, param) {
-	
-	if (parseInt($("#colMax").val()) <= parseInt($("#colMin").val())){
-		return false;
-	}
-	else{
-		return true;
-	}
+  
+  /* notifies the validator that an error exists */
+  if (parseInt($("#colMax").val()) <= parseInt($("#colMin").val())){
+    return false;
+  }
+  else{
+    return true;
+  }
 });
 
 /*Test to determine if row max > min*/
 $.validator.addMethod("rowTest", function(value, param) {
-	
-	if (parseInt($("#rowMax").val()) <= parseInt($("#rowMin").val())){
-		return false;
-	}
-	else{
-		return true;
-	}
+  
+  if (parseInt($("#rowMax").val()) <= parseInt($("#rowMin").val())){
+    return false;
+  }
+  else{
+    return true;
+  }
 });
 
 /*make sure document is ready*/
 $(document).ready(function() {
-	
-	/*Validate the form to check all field is valid*/
-	$("#inputForm").validate({
-
-	  rules: {
-		colMin: {
-		  required: true,
-		  min: -50,
-		  max: 50,
-		},
-		colMax: {
-		  required: true,
-		  min: -50,
-		  max: 50,
-		  colTest: true
-		},
-		rowMin: {
-		  required: true,
-		  min: -50,
-		  max: 50,
-		},
-		rowMax: {
-		  required: true,
-		  min: -50,
-		  max: 50,
-		  rowTest: true
-		}
-	  },
-	  messages: {
-		  colMin: {
-			  required: "<p>Please enter a number</p>",
-			  min: "<p>Number must be greater than -50</p>",
-			  max: "<p>Number must be lesser than 50</p>",
-		  },
-		  colMax: {
-			  required: "<p>Please enter a number</p>",
-			  min: "<p>Number must be greater than -50</p>",
-			  max: "<p>Number must be lesser than 50</p>",
-			  colTest: "<p>Must be greater than column Min"
-		  },
-		  rowMin: {
-			  required: "<p>Please enter a number</p>",
-			  min: "<p>Number must be greater than -50</p>",
-			  max: "<p>Number must be lesser than 50</p>",
-		  },
-		  rowMax: {
-			  required: "<p>Please enter a number</p>",
-			  min: "<p>Number must be greater than -50</p>",
-			  max: "<p>Number must be lesser than 50</p>",
-			  rowTest: "<p>Must be greater than row Min"
-		  },
-	  },
-	  // When all conditions are met, finally generate table
-	  submitHandler: function(form, e){
-		  e.preventDefault();
-		  removeOldTab(tabID); // removes old table
-		  generateTable(parseInt($("#colMin").val()),parseInt($("#colMax").val()),parseInt($("#rowMin").val()),parseInt($("#rowMax").val()),tabID);
-		}
-	});
+  
+  /*Validate the form to check all field is valid*/
+  $("#inputForm").validate({
+    /*Define rules to validate the input before data are submitted*/
+    rules: {
+      colMin: {
+        required: true,
+        min: -50,
+        max: 50,
+      },
+      colMax: {
+        required: true,
+        min: -50,
+        max: 50,
+        colTest: true
+      },
+      rowMin: {
+        required: true,
+        min: -50,
+        max: 50,
+      },
+      rowMax: {
+        required: true,
+        min: -50,
+        max: 50,
+        rowTest: true
+      }
+    },
+	/*Define error messages to tell user where errors occurred*/
+    messages: {
+      colMin: {
+        required: "<p>Please enter a number</p>",
+        min: "<p>Number must be greater than -50</p>",
+        max: "<p>Number must be lesser than 50</p>",
+      },
+      colMax: {
+        required: "<p>Please enter a number</p>",
+        min: "<p>Number must be greater than -50</p>",
+        max: "<p>Number must be lesser than 50</p>",
+        colTest: "<p>Must be greater than column Min"
+      },
+      rowMin: {
+        required: "<p>Please enter a number</p>",
+        min: "<p>Number must be greater than -50</p>",
+        max: "<p>Number must be lesser than 50</p>",
+      },
+      rowMax: {
+        required: "<p>Please enter a number</p>",
+        min: "<p>Number must be greater than -50</p>",
+        max: "<p>Number must be lesser than 50</p>",
+        rowTest: "<p>Must be greater than row Min"
+      },
+    },
+    /* When all conditions are met, finally generate table*/
+    submitHandler: function(form, e){
+      /*prevent page from refreshing when the user submits*/
+      e.preventDefault();
+	  
+	  /* removes old table*/
+      removeOldTab(tabID); 
+	  
+	  /*Call the function to generate table using the validated input from user
+	    Also parse the input values into integers*/
+      generateTable(parseInt($("#colMin").val()),parseInt($("#colMax").val()),parseInt($("#rowMin").val()),parseInt($("#rowMax").val()),tabID);
+    }
+  });
 });
 
 
@@ -108,7 +117,7 @@ function generateTable(cmin, cmax, rmin, rmax, table_id) {
   var totalCols = cmax - cmin + 1;
   
   /* To-do:
-  	1. Generate Column headers (x-axis)
+    1. Generate Column headers (x-axis)
     2. Generate Row headers (y-axis)
     3. Populate data inside the table using row and column headers as multipliers
   */
@@ -119,7 +128,7 @@ function generateTable(cmin, cmax, rmin, rmax, table_id) {
   
   //Generate column headers (x-axis)
   for(var i = 0; i < totalCols; i++){
-  	firstRow.insertCell(i+1).innerHTML = cmin + i;
+    firstRow.insertCell(i+1).innerHTML = cmin + i;
   }
   
   // Generate Row headers(y-axis) and populate data
@@ -131,7 +140,7 @@ function generateTable(cmin, cmax, rmin, rmax, table_id) {
     // for each row, also fill in all columns
     // populate/calculate data here
     for (var j = 0; j < totalCols; j++){
-    	//insert at cell j+1 because first cell is occupied by header
+      //insert at cell j+1 because first cell is occupied by header
         row.insertCell(j+1).innerHTML = rmin * (cmin+j);
     }
     rmin++; // increment row 
@@ -140,10 +149,10 @@ function generateTable(cmin, cmax, rmin, rmax, table_id) {
 
 // Removes old table so the new table can be set
 function removeOldTab(tabID){
-	var oldTab = document.getElementById(tabID);
-	while(oldTab.hasChildNodes()){
-		oldTab.removeChild(oldTab.firstChild); // remove all children of old table
-	}
+  var oldTab = document.getElementById(tabID);
+  while(oldTab.hasChildNodes()){
+    oldTab.removeChild(oldTab.firstChild); // remove all children of old table
+  }
 }
 
 
