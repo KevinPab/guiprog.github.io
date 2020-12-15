d3151d9d004ec3dfe4be862b00d6bce451a393c7
Paprawin Boonyakida <paprawin@hotmail.com>
2020-12-15 18:00:55 -0500

    updated hw8

--- js/hw8.js
+++ js/hw8.js
@@ -67,11 +67,23 @@ $(document).ready(function() {
   var i
   //populate index array to be used in picking and modifying actual tile datas
   for (i = 65; i <= 90; i++){
+    //push letters from A through Z to an index array
     indexArr.push(String.fromCharCode(i))
   }
   indexArr.push("_");
+  
+  //activates the drag/drop functionality of the jQuery UI
+  activateDragDrop();
 
-  //activate draggable/droppable items
+  //display the number of remaining tiles to the webpage for the user to see
+  get_num_remaining();
+
+  //resets the rack
+  reset_rack();
+});
+
+function activateDragDrop(){
+    //activate draggable/droppable items
   // this part of code was partially taken from https://jqueryui.com/droppable/#revert
   $(".draggable").draggable({
     revert:"invalid",
@@ -86,18 +98,22 @@ $(document).ready(function() {
         else if (!$(this).hasClass('occupied')) {
           // only allow user to place into adjacent board
           var boardNum = $(this)[0].id;
-          boardNum = boardNum.substr(boardNum.length -1); // gets last number representing the board string
 
-          //case first block
+          // gets last char in the string which represents the board spot's number
+          boardNum = boardNum.substr(boardNum.length -1); 
+
+          //case: first block
           if(boardNum == 1){
+            //check if the next block has a tile, if it has then allow tile to be placed on the first board spot
             var nextBoard = $("#board2");
             if(nextBoard.hasClass('occupied')){
               return true;
             }
           }
-          //case last block of the board
+          //case: last block of the board
           else if(boardNum == 7){
             var prevBoard = $("#board6");
+            //check if the previous board spot has a tile, if it has then allow tile to be placed on the last board spot
             if(prevBoard.hasClass('occupied')){
               return true;
             }
@@ -110,13 +126,12 @@ $(document).ready(function() {
             return true;
           }
           else{
-            //Record error message here
+            //else do not allow the tile to be placed
             return false;
           }
         }
         else{
           //else board is occupied, don't allow new tile to be placed
-          //could also print error message suggesting user to place to an adjacent tile on board
           return false;
         }
       },
@@ -126,9 +141,7 @@ $(document).ready(function() {
       },
       drop: function( event, ui ) {
 
-        //console.log("DROP ACCEPTED\n");
-
-        //here, 'this' refers to a droppable object (board)
+        //get the absolute position of the current board spot
         var abs_x = this.getBoundingClientRect().x;
         var abs_y = this.getBoundingClientRect().y;
 
@@ -136,48 +149,58 @@ $(document).ready(function() {
         // then, apply the absolute position
         var tile = ui.draggable[0];
 
+
+        //set the tile's position to 'this' board's absolute position so the tile is neatly placed onto the board
         $("#" + tile.id).css('position', 'absolute');
         $("#" + tile.id).css('left', abs_x + 'px');
         $("#" + tile.id).css('top', abs_y + 'px');
-
+        
+        //flag that this tile is currently being placed on the board
+        $("#" + tile.id).addClass('onboard');
+        
+        //prevent this letter tile from being moved after it has been placed
         $(ui.draggable[0]).draggable("disable"); //disable the current tile being dragged
 
         var curr_board = event.target;
 
+
+        //remove the class tag 'vacant'. this signifies that this board spot is not free anymore
         $(curr_board).removeClass("vacant");
+
         //flag that the board is occupied
         $(curr_board).addClass("occupied");
 
-        empty_board = false; // flag that a tile has been placed into the board
+        //set the global flag to know that an initial letter tile has been placed
+        empty_board = false;
 
-        //also update the current and total score
-        //get letter
+        //get this letter tile's character
         var tile_letter = $("#"+tile.id).attr('value');
+
         //find out how much this tile weights
         var tile_weight = ScrabbleTiles[tile_letter]["value"];
-
-        var score_with_bonus = 0;
         
-        //update both current and total score
+        //raw score in this round
         curr_score += parseInt(tile_weight);
 
-        //flag that a bonus is being used
+        //flag that the first double-word bonus is being used
         if(event.target.id == "board2"){
           first_bonus = true;
         }
+        //flag that the second double-word bonus is being used
         if(event.target.id == "board6"){
           second_bonus = true;
         }
 
-        //update current score to user, if the bonus is hit, update double the score
+        //update current score to user 
+        // if 2 bonuses are hit, make the score x4
         if(first_bonus && second_bonus){
           $("#actual_score").text(curr_score*4);
         }
-        //if only one of the bonuses is hit, only multiply by 2
+        //if only one of the bonuses is hit, only x2 the raw score
         else if(first_bonus || second_bonus){
           $("#actual_score").text(curr_score*2);
         }
-        //else no bonus was hit
+        //else no bonus was hit, so just use the raw score as the score
         else{
           $("#actual_score").text(curr_score);
         }
@@ -189,45 +212,18 @@ $(document).ready(function() {
         update_word_displayer();
 
         //keep track and show how many tiles are left
-        ScrabbleTiles[tile_letter]["number-remaining"]--;
+        //ScrabbleTiles[tile_letter]["number-remaining"]--;
         
-        //get number of remaining tiles (not placed onto the board)
+        //count and display number of tiles remaining to the webpage
         get_num_remaining();
- 
       }
     });
-  
-  get_num_remaining();
-
-  //for debugging
-  //console.log(remaining_tiles);
-
-  //resets the rack
-  reset_rack();
-});
-
-//displays the word currently played by the user
-function update_word_displayer(){
-  var i;
-  var myWord = ""; 
-  for (i = 1; i <=7; i++){
-    if($("#board" + i).hasClass('occupied')){
-      if ($("#board" +i ).attr('value') == "_"){
-        myWord += "-"; // represent space with a dash
-      }
-      else{
-        myWord += $("#board" +i ).attr('value');
-    }
-    }
-  }
-  $("#actual_word").text(myWord);
 }
 
 // generates 7 random tiles onto the rack
 function reset_rack(){
   var i;
   for (i = 1; i <= 7; i++){
-    var tileIndex = 65;
     //get random tile index
     var ranTile = indexArr[Math.floor(Math.random() * indexArr.length)];
     //console.log(ranTile);
@@ -235,55 +231,118 @@ function reset_rack(){
     //if this tile is available, insert. Otherwise, get another character tile
     if(ScrabbleTiles[ranTile]["number-remaining"] != 0){
 
-      //insert into page
-      //var tile = document.getElementById("tile" + i).src = "images/tiles/Scrabble_Tile_" + ranTile + ".jpg";
+      //insert image of the letter tile into page on the rack
       var t = $("#tile" + i);
       t.attr("src", "images/tiles/Scrabble_Tile_" + ranTile + ".jpg");
-      t.addClass("tid" + ranTile);
-      t.attr("value", ranTile); // add value
 
-      // keep track of how many of this tile remains
-      //ScrabbleTiles[ranTile]["number-remaining"]--;
+      //keep track of how many tile remains in the tile bag
+      ScrabbleTiles[ranTile]["number-remaining"]--;
+
+      //record the value of that tile
+      t.attr("value", ranTile);
     }
     else{
       i--; // don't count so the for loop and loop again
     }
+  }
+  get_num_remaining();
+}
 
-    //decrease the count of the tile
+//displays the word currently played by the user
+function update_word_displayer(){
+  var i;
+  var myWord = ""; 
+
+  //read the board and only read the spots containing a letter tile
+  for (i = 1; i <=7; i++){
+    if($("#board" + i).hasClass('occupied')){
+
+      //special exception for the underscore
+      if ($("#board" +i ).attr('value') == "_"){
+        myWord += "-"; // represent space with a dash
+      }
+      else{
+        //add each letter to a placeholder
+        myWord += $("#board" +i ).attr('value');
+    }
+    }
+  }
+  //display the string onto the webpage
+  $("#actual_word").text(myWord);
+}
+
+//change the tiles currently being played on the board
+//this only gets called when the user press submit
+function change_tiles(){
+  var i;
+  for (i = 1; i <= 7; i++){
+    //get random tile index
+    var ranTile = indexArr[Math.floor(Math.random() * indexArr.length)];
+
+    //only applied to the tile being played on the board
+    if($("#tile"+i).hasClass('onboard')){
+      //if this tile is available, insert. Otherwise, get another character tile
+      if(ScrabbleTiles[ranTile]["number-remaining"] != 0){
+
+        //add image of the letter tile
+        var t = $("#tile" + i);
+        t.attr("src", "images/tiles/Scrabble_Tile_" + ranTile + ".jpg");
+
+        //record the value of the tile
+        t.attr("value", ranTile); // add value
+
+        // reduce the count of that specific tile
+        ScrabbleTiles[ranTile]["number-remaining"]--; 
+        // remove the flag that the tile is onboard
+        t.removeClass('onboard'); 
+      }
+      else{
+        i--; // don't count so the for loop will loop again
+      }
+    }
   }
 }
 
 //clears and reset the class vacant in each of the board's placeholder
 function reset_board(){
   var i;
+
+  //for each spot of the board
   for (i = 1; i <= 7; i++){
     var board = $("#board" + i);
+
+    //do a clean reset of the vacant/occupied class tag
     board.removeClass("vacant");
     board.removeClass("occupied");
     board.addClass("vacant");
   }
-
+  //flag that the board is empty. This will allow the first tile to be placed anywhere
   empty_board = true; //flag that the board is empty again
 }
 
-//get number of tiles remaining
+//get number of tiles remaining and display it on screen
 function get_num_remaining(){
   var counter = 0;
   var i;
+  //count all the tiles remaining
   for (i = 0; i < indexArr.length; i++){
     counter += ScrabbleTiles[indexArr[i]]["number-remaining"];
-    //console.log(indexArr[i] + " has remaining" + ScrabbleTiles[indexArr[i]]["number-remaining"]);
   }
   remaining_tiles = counter;
+  //display onto the website
   $("#tiles_remaining").text(remaining_tiles);
-  //console.log("remaining:" + remaining_tiles);
 }
 
 //refill the missing tiles on the rack when the submit button is pressed
 $("#submit_btn").on("click", function() {
-  //update total score
+
+  //grab existing total score
   var tot = parseInt($("#total_score").text());
+
+  //add the current round's score to it
   tot += parseInt($("#actual_score").text());
+
+  //update/display total score to the webpage
   $("#total_score").text(tot);
 
   //reset current score
@@ -300,12 +359,31 @@ $("#submit_btn").on("click", function() {
     boardSpace.removeClass('occupied');
     boardSpace.addClass('vacant');
   }
-  //$( "#tile2" ).attr( "style", "position: relative" );
-  //$("#tile2").removeClass("ui-draggable-disabled");
+  $(".draggable").attr( "style", "position: relative" );
+  $(".draggable").removeClass("ui-draggable-disabled");
+
+  //reset the bonus flag and score at the end of each round
+  first_bonus = false;
+  second_bonus = false;
+  empty_board = true;
+  curr_score = 0;
+
+  //replace the tiles played while still keeping the ones that user didn't play
+  change_tiles();
+
+  //destroy the draggable tiles
+  $(".draggable").draggable("destroy");
+
+  //re-activate the draggable elements
+  activateDragDrop();
+
+  //count remaining tiles
+  get_num_remaining();
 
 });
 
 //restart the game, so reset everything
 $("#restart_btn").on("click", function() {
+  //refresh the page
   location.reload();
 });
